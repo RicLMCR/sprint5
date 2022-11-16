0c0ee40a2605df5435e90442dab693457cbacf8e
Ben-Mostyn <ben_mostyn@live.co.uk>
2022-11-16 09:01:32 +0000

    Component added for fetch Requests

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,42 +1,55 @@
 import { useState } from "react";
-import '../App.css';
+import "../App.css";
+import { getFetch } from "./FetchReq";
 
 // Pass data to app.tsx
 const Navbar = () => {
-    const [userName, setUsername] = useState("");
-    const [userPassword, setPassword] = useState("");
-    const [data, setData] = useState<any>("");
+  const [userName, setUsername] = useState("");
+  const [userPassword, setPassword] = useState("");
+  const [data, setData] = useState<any>("");
 
-    const handleSubmit = (e: any) => {
-        e.preventDefault();
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
 
-        fetch(`/api/users/userName/${userName}/userPassword/${userPassword}`)
-            .then((res) => res.json())
-            .then((data) => setData(data));
+    fetch(`/api/users/userName/${userName}/userPassword/${userPassword}`)
+      .then((res) => res.json())
+      .then((data) => setData(data));
 
-        console.log(data);
-    };
+    console.log(data);
+  };
 
-    // Post request to create an account..
+  // Post request to create an account..
 
-    return (
-        <div className="Nav-bar">
-            <form onSubmit={handleSubmit}>
-                <h1 className="h1-nav">Paid Time Off</h1>
-                <div className="form-group">
-                    <label>
-                        <input placeholder="User name" className="input-nav" type="text" value={userName} onChange={(event) => setUsername(event.target.value)} />
-                    </label>
-                    <label>
-                        <input placeholder="Password" className="input-nav" type="password" value={userPassword} onChange={(event) => setPassword(event.target.value)} />
-                    </label>
-                    <input className="button-nav" type="submit" value="Log in" />
-                    <input className="button-nav" type="submit" value="Register" />
-                </div>
-            </form>
-            <p>{data.userName}</p>
+  return (
+    <div className="Nav-bar">
+      <form onSubmit={handleSubmit}>
+        <h1 className="h1-nav">Paid Time Off</h1>
+        <div className="form-group">
+          <label>
+            <input
+              placeholder="User name"
+              className="input-nav"
+              type="text"
+              value={userName}
+              onChange={(event) => setUsername(event.target.value)}
+            />
+          </label>
+          <label>
+            <input
+              placeholder="Password"
+              className="input-nav"
+              type="password"
+              value={userPassword}
+              onChange={(event) => setPassword(event.target.value)}
+            />
+          </label>
+          <input className="button-nav" type="submit" value="Log in" />
+          <input className="button-nav" type="submit" value="Register" />
         </div>
-    );
-}
+      </form>
+      <p>{data.userName}</p>
+    </div>
+  );
+};
 
 export default Navbar;
