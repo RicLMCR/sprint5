05f74db719baf2feb960a0eedabe7d16cb57b240
RicLMCR <99919289+RicLMCR@users.noreply.github.com>
2022-11-21 15:19:01 +0000

    date range calc and rendering

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,23 @@ import "./App.css";
 import Navbar from "./components/navBar/Navbar";
 import { useState } from "react";
 import UserInfo from "./components/userInfo/UserInfo";
-import BookTO2 from "./components/bookTO/BookTO2";
+import BookTO from "./components/bookTO/BookTO";
 import SamsComponent from "./components/samsComponent/SamsComponent";
 
-
 function App() {
+  //user information retrieval
   const [data, setData] = useState<any>("");
-  const [bookingArray, setBookingArray] = useState<any>([]);
-  console.log(bookingArray, "Booking state in App");
+
+  //booking information
+  const [dateOne, setDateOne] = useState<Date>();
+  const [dateTwo, setDateTwo] = useState<Date>();
 
   console.log(data);
   return (
     <div className="App">
       <Navbar setData={setData} data={data} />
       <UserInfo toAllowance={data} />
-
-      <BookTO2 />
+      <BookTO />
     </div>
   );
 }
