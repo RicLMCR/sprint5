0a8954e5c0784152b63109a31e0f3cff364827e5
Louis Nokes <79872690+LouisNokes@users.noreply.github.com>
2022-12-08 14:02:50 +0000

    Fixed containers

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,14 +1,12 @@
 import App from '../App';
 import renderer from 'react-test-renderer'
+import { render } from '@testing-library/react';
 
-/* describe('Tests that the app its self renders correctly', () => {
+describe('Tests that the app its self renders correctly', () => {
 
   it(' App renders correctly', () => {
-    const tree = renderer
-      .create(<App />)
-      .toJSON()
-    expect(tree).toMatchSnapshot()
+    render(<App />)
+    //  .toJSON()
+    // expect(tree).toMatchSnapshot()
   })
-
-
-}) */
+})
