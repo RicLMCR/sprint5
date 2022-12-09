113355c031a76399c256f25303d68f3a03588604
RicLMCR <99919289+RicLMCR@users.noreply.github.com>
2022-12-09 08:51:33 +0000

    working pipeline

--- scripts/runAll.sh
+++ scripts/runAll.sh
@@ -16,19 +16,22 @@ cd ..
 
 printf "\nPull for backend completed" >> $log
 
+open http://localhost:3000
+
 docker-compose up
 
 echo -e " Completed Pull from frontend\n Completed Pull from backend\n Completed Docker Compose process"
 
 printf "\nDocker Compose has completed" >> $log
 
-# docker exec -ti frontend sh -c "npm run testauto"
-
 if [ -n "$(docker ps -f "name=reactapp_c" -f "status=running" -q )" ]; then
     echo "the container is running!"
     sleep 5
     open http://localhost:3000
 fi
+
+# Confirm tests passed
+printf "\nTests completed" >> $log
 
 #!How to run this script
     # in root directory 
